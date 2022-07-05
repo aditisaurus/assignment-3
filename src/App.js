849169e0c5c466f4ac51a26806cb786a086b48c9
aditisinghdeveloper <aditisinghdeveloper@gmail.com>
2022-07-05 14:44:20 +0530

    Setting up Card

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <Main/>
+      <Main data={data} setData={setData}/>
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import './Main.css';
+import {Card, Button} from 'react-bootstrap';
+import {PhoneOutlined, MailOutlined, GlobalOutlined } from '@ant-design/icons';
 
-function Main() {
+function Main({data, setData}) {
   return (
-    <div>Main</div>
+    <div className="main">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <h3 className="name">Name</h3>
+        <div className="mail">
+        <MailOutlined className="icon"/>
+        <p>Mail</p>
+        </div>
+        <div className="phone">
+        <PhoneOutlined className="icon" /> 
+        <p>Phone</p>
+        </div>
+        <div className="web">
+        <GlobalOutlined className="icon" /> 
+        <p>Web</p>
+        </div>
+      </Card.Body>
+    </Card>
+    </div>
   )
 }
 
